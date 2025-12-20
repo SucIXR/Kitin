94ef95a4cef73c742f45ef634709072140ce5893
SucIXR <sasasa123456@foxmail.com>
2025-12-21 00:18:49 +0800

    github-actions

--- scripts/SetENV.sh
+++ scripts/SetENV.sh
@@ -1,6 +1,28 @@
-echo "project_id=kitin" >> $GITHUB_ENV
-echo "project_id_b=Kitin" >> $GITHUB_ENV
-echo "mcversion=1.21.1" >> $GITHUB_ENV
-echo "tag=$(date +'%Y%m%d%H%M')" >> $GITHUB_ENV
-echo "commit_id=$(git rev-parse --short HEAD)" >> $GITHUB_ENV
-echo "jar_dir=kitin-server/build/libs/*-paperclip-*-mojmap.jar" >> $GITHUB_ENV
+prop() {
+  grep "^[[:space:]]*${1}" gradle.properties | cut -d'=' -f2 | sed 's/^[[:space:]]*//; s/\r//'
+}
+
+project_id="kitin"
+project_id_b="Kitin"
+
+commitid=$(git log --pretty='%h' -1)
+mcversion=$(prop mcVersion)
+grdversion=$(prop version)
+preVersion=$(prop preVersion)
+release_tag="$mcversion-$commitid"
+jarName="$project_id-$mcversion-$commitid"
+jarName_dir="kitin-server/build/libs/$jarName.jar"
+make_latest=$([ $preVersion = "true" ] && echo "false" || echo "true")
+
+mv kitin-server/build/libs/$project_id-paperclip-$grdversion-mojmap.jar $jarName_dir
+
+echo "project_id=$project_id" >> $GITHUB_ENV
+echo "project_id_b=$project_id_b" >> $GITHUB_ENV
+echo "commit_id=$commitid" >> $GITHUB_ENV
+echo "commit_msg=$(git log --pretty='> [%h] %s' -1)" >> $GITHUB_ENV
+echo "mcversion=$mcversion" >> $GITHUB_ENV
+echo "pre=$preVersion" >> $GITHUB_ENV
+echo "tag=$release_tag" >> $GITHUB_ENV
+echo "jar=$jarName" >> $GITHUB_ENV
+echo "jar_dir=$jarName_dir" >> $GITHUB_ENV
+echo "make_latest=$make_latest" >> $GITHUB_ENV
