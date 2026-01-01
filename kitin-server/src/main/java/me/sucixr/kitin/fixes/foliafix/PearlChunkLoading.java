b4d96a3333de862e4212429b763fad98cec58fb7
SucIXR <138633538+SucIXR@users.noreply.github.com>
2026-01-02 04:57:54 +0800

    up text

--- kitin-server/src/main/java/me/sucixr/kitin/fixes/foliafix/PearlChunkLoading.java
+++ kitin-server/src/main/java/me/sucixr/kitin/fixes/foliafix/PearlChunkLoading.java
@@ -7,18 +7,18 @@ import net.minecraft.world.level.storage.ValueOutput;
 import static me.sucixr.kitin.scheduler.data.PearlSnapshot.PEARL_DATA_MAP;
 
 /*
-环节	    原版逻辑	             Folia的限制手段	             后果
-运行时	珍珠跨区读取 Player对象  ServerLevel禁止跨区查找实体	珍珠以为玩家离线，停止加载区块
-存盘时	玩家 NBT 记录珍珠坐标	 ServerPlayer 移除珍珠注册列表	下线不保存珍珠数据，珍珠丢失
-读盘时	玩家上线加载珍珠区块	 玩家 NBT 为空，不发 Ticket	珍珠实体在硬盘里沉睡，不复活
-传送时	珍珠直接修改玩家坐标	 Entity 抛出异常或线程报错	    无法实现跨区传送
+环节	    原版逻辑	             Folia的限制手段	                后果
+运行时	珍珠跨区读取 Player对象  ServerLevel禁止跨区查找实体	    珍珠以为玩家离线，被Folia严格的owner != null检测抹杀
+存盘时	玩家 NBT 记录珍珠坐标	ServerPlayer 移除珍珠注册列表	下线不保存珍珠数据，珍珠丢失
+读盘时	玩家上线加载珍珠区块	   玩家 NBT 为空，不发 Ticket	    珍珠的生成就是靠NBT，没有就不生成
+传送时	珍珠直接修改玩家坐标	   通过异步传送	                    暂未发现
 
-总结:Folia几乎在源码的各个地方都大刀阔斧地阻止了实体跨线程访问!如果直接修复原有珍珠方法几乎不可能
+总结:Folia几乎在源码的各个地方都大刀阔斧地阻止了实体跨线程访问!owner != null检测遍地都是,如果直接修复原有珍珠方法几乎不可能
 
 实现原理:
-特性        Folia               Kitin                    效果
-玩家查找    查当前 Region(局部)   查 PlayerList (全局)       无视跨区，永不丢失目标
-传送执行    依赖当前线程          调度到目标线程执行            绕过线程安全检查，合法传送
+特性        Folia               Kitin                        效果
+玩家查找    查当前 Region(局部)   查 PlayerList (全局)        无视跨区，永不丢失目标
+传送执行    异步                  同Folia                     Folia本身珍珠传送就是异步的，意义何在
 区块加载    依赖玩家对象存在       无条件自我维持               [下线不消失，区块常驻]
 
 以下内容是为了解决"永不丢失目标导致的玩家退出游戏后珍珠依旧存在"而进行的修复
